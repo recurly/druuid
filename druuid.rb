faa6cb97783dff07b06a26835cde8a421788745f
Drew Kitch <drew.kitch@gmail.com>
2016-03-13 17:05:26 -0600

    gen_range for scoping datetime ranges with id

--- druuid.rb
+++ druuid.rb
@@ -38,6 +38,39 @@ module Druuid
       Time.at (ms / 1e3) + epoch_offset.to_i
     end
 
+    # Provides lower bound id based on datetime provided.
+    #
+    # @param [Time] time of lower bound id
+    # @return [BigNum] Lower Bound ID
+    # @example
+    #   Druuid.gen_lower_bound Time.utc(2015,2,3,12,2,34)
+    # #=> 11936695196844032000
+    def gen_lower_bound time
+      gen(time) >> (64 - 41) << (64 - 41)
+    end
+
+    # Provides upper bound id based on datetime provided.
+    #
+    # @param [Time] time of upper bound id
+    # @return [BigNum] Upper Bound ID
+    # @example
+    #   Druuid.gen_upper_bound Time.utc(2015,2,3,17,56,12)
+    # #=> 11936873186336964607
+    def gen_upper_bound time
+      gen(time) | 0b111_1111_1111_1111_1111_1111
+    end
+
+    # Provides lower and upper bound ids based on datetime range provided.
+    #
+    # @param [Range] datetime range for upper and lower bound ids
+    # @return [Range] ids of lower and upper bound range
+    # @example
+    #   Druuid.gen_range Time.utc(2015,2,3,12,2,34), Time.utc(2015,2,3,17,56,12)
+    #   # => 11936695196844032000..11936873186336964607
+    def gen_range datetime_range
+      gen_lower_bound(datetime_range.begin)..gen_upper_bound(datetime_range.end)
+    end
+
   end
 
 end
